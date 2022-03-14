d09a32ce182bd523e9c5a6b29f898b0966099785
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-03-14 14:41:01 -0300

    Validations

--- basic/todomvc-tests/cypress/integration/todomvc.spec.js
+++ basic/todomvc-tests/cypress/integration/todomvc.spec.js
@@ -9,12 +9,22 @@ it('should be able to add a new todo to the list', () => {
 
   cy.get('.new-todo'/*, {timeout: 6000} */).type('Clean room{enter}')
 
-  cy.get('.toggle').click()
-
-  cy.contains('Clear completed').click()
+  cy.get('label').should('have.text', 'Clean room')
+  cy.get('.toggle').should('not.be.checked')
 })
 
-it('should be able to clear completed tests', () => {
+it('should be able to complete a task', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+  cy.get('.new-todo').type('Clean room{enter}')
+
+  cy.get('.toggle').click()
+
+  cy.get('.toggle').should('be.checked')
+  cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+})
+
+it('should be able to clear completed tasks', () => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/')
 
   cy.get('.new-todo').type('Clean room{enter}')
@@ -22,4 +32,6 @@ it('should be able to clear completed tests', () => {
   cy.get('.toggle').click()
 
   cy.contains('Clear completed').click()
+
+  cy.get('.todo-list').should('not.have.descendants', 'li')
 })
