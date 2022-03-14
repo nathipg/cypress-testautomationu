cb1c6d2dd5179148b00c22620421a1df043d0030
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-03-14 14:32:25 -0300

    Access and interect with elements

--- basic/todomvc-tests/cypress/integration/todomvc.spec.js
+++ basic/todomvc-tests/cypress/integration/todomvc.spec.js
@@ -1,5 +1,25 @@
 /// <reference types="cypress" />
 
 it('should navigate to the TodoMVC App', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/') // ?delay-new-todo=5000
+})
+
+it('should be able to add a new todo to the list', () => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/')
-});
+
+  cy.get('.new-todo'/*, {timeout: 6000} */).type('Clean room{enter}')
+
+  cy.get('.toggle').click()
+
+  cy.contains('Clear completed').click()
+})
+
+it('should be able to clear completed tests', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+  cy.get('.new-todo').type('Clean room{enter}')
+
+  cy.get('.toggle').click()
+
+  cy.contains('Clear completed').click()
+})
