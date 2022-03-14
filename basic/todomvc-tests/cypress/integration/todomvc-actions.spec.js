2c7960e66c17c8b8fca6291981fe4e7364212a5d
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-03-14 15:39:20 -0300

    Page Objects

--- basic/todomvc-tests/cypress/integration/todomvc-actions.spec.js
+++ basic/todomvc-tests/cypress/integration/todomvc-actions.spec.js
@@ -1,31 +1,39 @@
 /// <reference types="cypress" />
 
+import {
+  addTodo,
+  navigate,
+  validateTodoText,
+  validateToggleState,
+  toggleTodo,
+  validateTodoCompletedState,
+  clearCompleted,
+  validateNumberOfTodosShown,
+} from '../page-objects/todo-page';
+
 describe('todo actions', () => {
-
   beforeEach(() => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/') // ?delay-new-todo=5000
-
-    cy.get('.new-todo'/*, {timeout: 6000} */).type('Clean room{enter}')
-  })
+    navigate();
+    addTodo('Clean room');
+  });
 
   // it.only => run a single test
-  
-  it('should be able to add a new todo to the list', () => {  
-    cy.get('label').should('have.text', 'Clean room')
-    cy.get('.toggle').should('not.be.checked')
-  })
-  
-  it('should be able to complete a task', () => {
-    cy.get('.toggle').click()
-  
-    cy.get('.toggle').should('be.checked')
-    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
-  })
-  
-  it('should be able to clear completed tasks', () => {
-    cy.get('.toggle').click()
-    cy.contains('Clear completed').click()
-  
-    cy.get('.todo-list').should('not.have.descendants', 'li')
-  })
-})
+
+  it('should be able to add a new todo to the list', () => {
+    validateTodoText(0, 'Clean room');
+    validateToggleState(0, false);
+  });
+
+  describe('toggling todos', () => {
+    it('should be able to complete a task', () => {
+      toggleTodo(0);
+      validateTodoCompletedState(0, true);
+    });
+
+    it('should be able to clear completed tasks', () => {
+      toggleTodo(0);
+      clearCompleted();
+      validateNumberOfTodosShown(0);
+    });
+  });
+});
